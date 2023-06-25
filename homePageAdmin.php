2de5193f760329172afe8f8628ee81461745f174
TeaBegaj17 <tbegaj21@epoka.edu.al>
2023-06-25 23:08:37 +0200

    'commit'

--- homePageAdmin.php
+++ homePageAdmin.php
@@ -6,12 +6,16 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" type="text/css" href="assets/css/styleHomeAdmin.css">
-    <script src="https://kit.fontawesome.com/132b724676.js" crossorigin="anonymous"></script> 
+    <script src="https://kit.fontawesome.com/132b724676.js" crossorigin="anonymous"></script>
+    
+    
+    
 </head>
 <body>
 
