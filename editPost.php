108af00df51c4b7e539b381b3235bc988cbc6f68
Huerta <hkalaci21@epoka.edu.al>
2023-06-27 19:30:50 +0200

    Commit

--- editPost.php
+++ editPost.php
@@ -19,47 +19,57 @@ if(isset($_GET ['id'])){
   <title> Add blog post</title>
     <link rel="stylesheet" href="./assets/css/styleBlogPostAdmin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  
+    <script src="https://kit.fontawesome.com/132b724676.js" crossorigin="anonymous"></script> 
 </head>
 <body>
    
-  <div class="sidebar">
+<div class="sidebar">
     <div class="icon-container">
       <div class="logo-sidebar">
         <img src="image/Logo.png" alt="Logo" class="logo">
         <img src="image/PharmaLineNameLogo.png" alt="Logo" class="nameLogo">
       </div>
       <div class="total-icons">
-        <div class="icon" id="Home">
-          <i class="fas blog"></i>
-          <img src="image/homeIcon.jpg" alt="Home Icon">
-          <span class="icon-name">Home</span>
-        </div>
-        <div class="icon" id="Check-sales">
-          <i class="fas check-sales"></i>
-          <img src="image/revenueIcon.png" alt="Sales Icon">
-          <span class="icon-name">Check Sales</span>
-        </div>
+      
+      <div class="icon" id="Home">
+      <a href="homePageAdmin.php">
+      <i class="fa-solid fa-house" style="color: #21324f;"></i>
+      <span class="icon-name">Home</span>
+      </a>
+      </div>
+     
+   
         <div class="icon" id="Generate-discount">
-          <i class="fas generate-dis"></i>
-          <img src="image/generateCodeIcon.png" alt="Discount Icon">
+         <a href="generateDiscount.php">
+        <i class="fa-solid fa-tags" style="color: #0c182c;"></i>
           <span class="icon-name">Generate Discount</span>
+         </a>
         </div>
 
-        <div class="icon" id="ManageProducts">
-          <a href="manageProducts.php">
-          <i class="fa-solid fa-list-check" style="color: #25395b;"></i>
-          <span class="icon-name">Manage Products</span></a>
+      <a href="blogAdmin.php">
+        <div class="icon" id="Blog">
+          <i class="fas blog"></i>
+          <img src="image/logoIcon.png" alt="Blog Icon">
+          <span class="icon-name">Blog</span>
         </div>
-
+      </a>
+        <div class="icon" id="Manage Products">
+        <a href="manageProducts.php">
+        <i class="fa-solid fa-list-check" style="color: #25395b;"></i>
+          <span class="icon-name">Manage Products</span>
+         </a>
+        </div>
+        
         <div class="icon" id="Log-out">
-          <i class="fas log-out"></i>
-          <img src="image/user (1).png" alt="Bottom Icon">
-          <span class="icon-name">Log out</span>
+          <a href="users/logout.php">
+          <i class="fa-solid fa-right-from-bracket" style="color: #222d3f;"></i>
+          <span class="icon-name">Log Out</span>
+         </a>
         </div>
         </div>
     </div>
   </div>
+
 
 
 <section class="form__section">
